9e726dcd2227fdd49cdd6e0b51e074ce003dc4ff
John Melendy <melja@pobox.com>
2024-01-24 22:09:26 -0800

    Proto-spa and delete author

--- manage/content.py
+++ manage/content.py
@@ -8,6 +8,11 @@ from manage.db import get_db
 
 bp = Blueprint('content', __name__, url_prefix='/content')
 
+@bp.route("/", methods=["GET"])
+@login_required
+def content():
+    return render_template('content/content.html')
+
 @bp.route("/authors", methods=["GET", "POST"])
 @login_required
 def authors():
@@ -35,8 +40,29 @@ def authors():
         return redirect(url_for('content.authors'))
 
     authors = db.execute(
-        'SELECT name'
+        'SELECT id, name'
         ' FROM authors'
         ' ORDER BY name'
     ).fetchall()
     return render_template('content/authors.html', authors=authors)
+
+# def get_author(id):
+#     author = get_db().execute(
+#         'SELECT a.id, a.name, a.creation_date'
+#         ' FROM authors a WHERE a.id = ?',
+#         (id,)
+#     ).fetchone()
+
+#     if author is None:
+#         abort(404, f"Author id {id} doesn't exist.")
+
+#     return author
+
+@bp.route('/delete_author/<int:id>', methods=('POST',))
+@login_required
+def delete_author(id):
+    # author = get_author(id)
+    db = get_db()
+    db.execute('DELETE FROM authors WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('content.authors'))#, author=author)
